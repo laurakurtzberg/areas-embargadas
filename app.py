b112ac9ab97a712e5b1ce788c7267c7dfe009806
laurakurtzberg <laura.kurtzberg@gmail.com>
2014-03-31 18:25:58 -0300

    Added a detail page for each person

---
+++ app.py
@@ -0,0 +1,29 @@
+import csv
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+csv_path = './static/areas-embargadas2.csv'
+csv_obj = csv.DictReader(open(csv_path, 'r'))
+csv_list = list(csv_obj)
+csv_dict = dict([[o['numero_tad'], o] for o in csv_list])
+
+@app.route("/")
+def index():
+    return render_template('index.html',
+        object_list=csv_list,
+    )
+
+@app.route('/<number>/')
+def detail(number):
+    return render_template('detail.html',
+        object=csv_dict[number],
+    )
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        use_reloader=True,
+        debug=True,
+    )
