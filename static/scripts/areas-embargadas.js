534e5a2ef3f75d5d492ccd50c2bed192f0b364ea
Kit Cross <kit@kitx.net>
2014-04-03 12:45:00 -0300

    Styling updates to the table

--- static/scripts/areas-embargadas.js
+++ static/scripts/areas-embargadas.js
@@ -1,3 +1,11 @@
-var map = L.mapbox.map('map', 'infoamazonia.hm6a0hke').setView([-7.245, -52.452], 4);
-// var layer = L.mapbox.map('infoamazonia.hm5iceem');
-// map.addLayer(layer);
+$(document).ready(function() {
+
+	var map = L.mapbox.map('map', 'infoamazonia.hm6a0hke').setView([-7.245, -52.452], 5);
+	// var layer = L.mapbox.map('infoamazonia.hm5iceem');
+	// map.addLayer(layer);
+
+	$(".areas-table").tablesorter(); 
+
+	console.log('page loaded');
+
+});
